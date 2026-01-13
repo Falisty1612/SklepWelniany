f9070dff35ca72f0c7bcfa6902af1ab2a5c5683b
sushiimilk <151689014+sushiimilk@users.noreply.github.com>
2026-01-14 00:10:10 +0100

    proba naprawy

--- SklepWelniany/Repositories/UserOrderRepository.cs
+++ SklepWelniany/Repositories/UserOrderRepository.cs
@@ -19,7 +19,7 @@ namespace SklepWelniany.Repositories
 
         public async Task ChangeOrderStatus(UpdateOrderStatusModel data)
         {
-             var order = await _db.Orders.FindAsync(data.OrderId);
+            var order = await _db.Orders.FindAsync(data.OrderId);
             if (order == null)
             {
                 throw new InvalidOperationException($"Order with ID {data.OrderId} not found.");
