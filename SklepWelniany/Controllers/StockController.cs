b66971ee33dce13ca90313bcd303acb0c57eef13
sushiimilk <151689014+sushiimilk@users.noreply.github.com>
2026-01-14 02:33:27 +0100

    Stock ciag dalszy

---
+++ SklepWelniany/Controllers/StockController.cs
@@ -0,0 +1,52 @@
+﻿using Humanizer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SklepWelniany.Controllers
+{
+    [Authorize(Roles=nameof(Roles.Admin))]
+    public class StockController : Controller
+    {
+        private readonly IStockRepository _stockRepo;
+
+        public StockController(IStockRepository stockRepo)
+        {
+            _stockRepo = stockRepo;
+        }
+
+        public async Task<IActionResult> Index(string sTerm = "")
+        {
+            var stocks = await _stockRepo.GetStocks(sTerm);
+            return View(stocks);
+        }
+
+        public async Task<IActionResult> ManageStock(int productId)
+        {
+            var existingStock = await _stockRepo.GetStockByProductId(productId);
+            var stock = new StockDTO
+            {
+                ProductId = productId,
+                Quantity = existingStock != null ? existingStock.Quantity : 0
+            };
+            return View(stock);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ManageStock(StockDTO stock)
+        {
+            if (!ModelState.IsValid)
+                return View(stock);
+            try
+            {
+                await _stockRepo.ManageStock(stock);
+                TempData["successMessage"] = "Magazyn zaktualizowany pomyślnie.";
+            }
+            catch (Exception ex)
+            {
+                TempData["errorMessage"] = $"Wystąpił błąd podczas aktualizacji magazynu: {ex.Message}";
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
--- SklepWelniany/Models/DTOs/StockDTO.cs
+++ SklepWelniany/Models/DTOs/StockDTO.cs
@@ -4,7 +4,7 @@ namespace SklepWelniany.Models.DTOs
 {
     public class StockDTO
     {
-        public int BookId { get; set; }
+        public int ProductId { get; set; }
         [Range(0, int.MaxValue, ErrorMessage = "Quantity must be a non-negative value.")]
         public int Quantity { get; set; }
     }
